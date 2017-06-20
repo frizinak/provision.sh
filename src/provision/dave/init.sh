e6f8477401343939b9c29d6dd4316069cdffcbe6
Kobe Lipkens <kobelipkens@gmail.com>
2017-06-20 22:06:29 +0200

    Dave + node

--- src/provision/dave/init.sh
+++ src/provision/dave/init.sh
@@ -6,13 +6,20 @@ EOF
 include web
 include node
 
+install openssl libgcrypt11-dev
+
 ufw allow 1337
 
 ensure_dir "${docroot}"
 if [ ! -d "${docroot}/dave/.git" ]; then
-    git clone https://klipkens@bitbucket.org/wieni/deploy.git "${docroot}/dave"
-    make -C "${docroot}/dave"
+    git clone https://klipkens@wieni.githost.io/wieni/deploy "${docroot}/dave"
 fi
+
+# make -C "${docroot}/dave" reset
+sudo -u "${webuser}" -i make -C "${docroot}/dave"
+
+rm -rf "${docroot}/dave/resources"
+mv "$1/resources" "${docroot}/dave/resources"
 
 fix_user_perms "${webuser}"
 
--- src/provision/node/init.sh
+++ src/provision/node/init.sh
@@ -3,8 +3,13 @@ help_text <<EOF
 nodejs
 EOF
 
-if ! node -v  | grep 'v6\..\..'; then
-    chmod +x "$1/setup6.x.sh"
-    "$1/setup6.x.sh"
+if ! node -v  | grep 'v6\.9\.1'; then
+    curl -o- "https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh" \
+        | bash
+    . ~/.nvm/nvm.sh
+    nvm install 6.9.1
+    cp -r ~/.nvm/versions/node/v6.9.1/bin/* /usr/bin/
 fi
-install nodejs node-gyp
+
+node-gyp &>/dev/null || npm -g install node-gyp
+which yarn &>/dev/null || npm -g install yarn
