c585567437c2b4ed8786e00416fdf2da507363bd
Kobe Lipkens <kobelipkens@gmail.com>
2018-05-02 16:28:10 +0200

    Provisioners: swap and xdebug

--- src/provision/swap/init.sh
+++ src/provision/swap/init.sh
@@ -1,10 +1,15 @@
 #! /bin/bash
 help_text <<EOF
 Create a temp swap file
- - arg: size in MiB [default: 1024]
+ - arg: size in MiB, 0 to remove swap [default: 1024]
 EOF
 
 swapoff /swap 2>/dev/null || true
+if [ "$2" == "0" ]; then
+    rm /swap || true
+    return
+fi
+
 count=$(( ${2:-1024} * 1024 ))
 dd if=/dev/zero of=/swap bs=1024 count=$count
 chown root:root /swap
---
+++ src/provision/xdebug/init.sh
@@ -0,0 +1,7 @@
+help_text <<EOF
+PHP XDebug
+ - arg: php version
+EOF
+
+install php${2}-xdebug
+cp "${1}/xdebug.ini"  "/etc/php/${2}/mods-available/xdebug.ini"
