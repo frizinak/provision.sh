a95a71e0b0312ebf6335172c0fa45efdd03814b0
Kobe Lipkens <kobelipkens@gmail.com>
2018-07-29 09:43:02 +0200

    Speed up xdebug install if php<version>-xdebug does not exist

--- src/provision/xdebug/init.sh
+++ src/provision/xdebug/init.sh
@@ -3,7 +3,9 @@ PHP XDebug
  - arg: php version
 EOF
 
-install php${2}-xdebug
+if ! dpkg --status "php${2}-xdebug" &>/dev/null && ! dpkg --status "php-xdebug" &>/dev/null; then
+    install php${2}-xdebug 2>/dev/null || install php-xdebug
+fi
 cp "${1}/xdebug.ini"  "/etc/php/${2}/mods-available/xdebug.ini"
 
 phpenmod -v ${2} xdebug
