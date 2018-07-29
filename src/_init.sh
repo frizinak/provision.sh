9530619319940d3ac79755bbeb2fa43ecdee31a6
Kobe Lipkens <kobelipkens@gmail.com>
2018-07-29 09:43:02 +0200

    Fix _init.sh error cathing and prettify output

--- src/_init.sh
+++ src/_init.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
-set -e
 sed "1,/^# START TAR #$/d" "$0" | tar -xz
 find provision -type f -exec chmod 600 {} \;
 find provision -type d -exec chmod 700 {} \;
@@ -21,10 +20,16 @@ rm -rf provision
 rm "$0";
 
 if [ $stat -eq 0 ]; then
-    echo 'success'
+    echo
+    echo -e '\033[1;30;42m         \033[0m'
+    echo -e '\033[1;30;42m SUCCESS \033[0m'
+    echo -e '\033[1;30;42m         \033[0m'
     exit 0
 fi
 
-echo 'FAILED!';
+echo
+echo -e '\033[1;37;41m        \033[0m'
+echo -e '\033[1;37;41m FAILED \033[0m'
+echo -e '\033[1;37;41m        \033[0m'
 exit $stat
 # START TAR #
