27ea7fe6ea600053b978ce80cff472a59e94c524
Kobe Lipkens <kobelipkens@gmail.com>
2019-06-18 15:31:28 +0200

    ditch problematic oracle jre

--- src/provision/elasticsearch/init.sh
+++ src/provision/elasticsearch/init.sh
@@ -6,7 +6,7 @@ if arg 1 is 'public' elastic will be reachable on 9200
 arg 2 is the java heapsize in mb (default: 128)
 EOF
 
-include java
+install default-jre
 
 if ! dpkg --status "elasticsearch" >/dev/null 2>&1; then
     wget -O /tmp/elastic.deb 'https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.6.10.deb'
